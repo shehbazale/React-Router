40e8b04ab3fe33f18a54c5dd68887a7db4a06129
BasitAyaz <basitahmed1997@gmail.com>
2024-05-19 13:46:49 +0500

    (19-05-2024) Quiz App, API Handeling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import QuizScreen from './pages/QuizScreen';
+import APIHandeling from './pages/API HandelingScreen';
 
 function App() {
 
 
-  return <QuizScreen />;
+  return <APIHandeling />;
 }
 
 export default App;
--- src/pages/API HandelingScreen.tsx
+++ src/pages/API HandelingScreen.tsx
@@ -1,3 +1,70 @@
+import axios from "axios"
+import { useState } from "react"
+
 export default function APIHandeling() {
-    return
+
+    const [dataList, setDataList] = useState<any>([])
+
+
+    const callApi = () => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((res) => {
+                console.log(res.data)
+                setDataList([...res.data])
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const postData = () => {
+        axios.post('https://jsonplaceholder.typicode.com/posts', {
+            userId: 1,
+            title: "Something Important",
+            body: "ajsdk jas lkdjak la la lkdalk dlsa lkdsaj"
+        }).then(res => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+    const editData = () => {
+        axios.put('https://jsonplaceholder.typicode.com/posts/2', {
+            userId: 1,
+            id: 2,
+            title: "Something Important",
+            body: "ajsdk jas lkdjak la la lkdalk dlsa lkdsaj"
+        }).then(res => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+    const deleteData = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/2')
+            .then(res => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
+
+
+    return <>
+        <button onClick={callApi}>Call API</button>
+        <button onClick={postData}>Post Data</button>
+        <button onClick={editData}>Edit Data</button>
+        <button onClick={deleteData}>Delete Data</button>
+
+        <table className="table">
+            <tbody>
+                {dataList.map((x: any, i: number) => <tr>
+                    <td>{x.name}</td>
+                    <td>{x.userName}</td>
+                    <td>{x.website}</td>
+                    <td>{x.email}</td>
+                </tr>)}
+            </tbody>
+        </table>
+    </>
 }
